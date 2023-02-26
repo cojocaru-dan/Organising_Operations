dadd7f55905af9751da1b822654078dd01fdba84
cojocaru-dan <dan.cojocaru90233@gmail.com>
2023-02-26 12:11:54 -0800

    work with functions

--- data.js
+++ data.js
@@ -1,13 +1,45 @@
 //Your code comes here
+let thisText = "global_variable";
 
+function logIntoTerminal(toLog, secondToLog){
+    console.log(thisText);
+    let localText = "local_variable";
+    console.log(localText);
 
+    function fourthFunction(){
+        console.log("inside the fourthFunction function");
+    }
+    fourthFunction();
+    console.log(toLog);
+    console.log(secondToLog);
+}
 
+let a = 3;
+logIntoTerminal("first argument", a);
 
+function anotherFunction (){
+    let b = 47;
+    logIntoTerminal("argument to use as parameter inside function", b);
+}
 
+anotherFunction();
 
+const thirdFunction = function (){
+    let c = 81;
+    logIntoTerminal("third argument", c); 
+}
 
+thirdFunction();
 
+function greetings(firstName){
+    return "Greetings, " + firstName;
+}
 
+console.log(greetings("Sophia"));
+console.log(greetings("Robert"));
+console.log(greetings("Charlotte"));
+console.log(greetings("Amelia"));
+console.log(greetings("Benjamin"));
 
 
 
